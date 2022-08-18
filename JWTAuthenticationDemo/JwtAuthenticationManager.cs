2fd71e80e6bcfaa02abbe326401459312865c2c6
pawarganesh <pawarganesh5555@gmail.com>
2022-08-18 22:08:58 +0530

    Update Time Stamp 20 Sec to 15 Sec

--- JWTAuthenticationDemo/JwtAuthenticationManager.cs
+++ JWTAuthenticationDemo/JwtAuthenticationManager.cs
@@ -39,7 +39,7 @@ namespace JWTAuthenticationDemo
                 {
                     new Claim(ClaimTypes.Name, username)
                 }),
-                Expires = DateTime.UtcNow.AddSeconds(20),
+                Expires = DateTime.UtcNow.AddSeconds(15),
                 SigningCredentials = new SigningCredentials(
                                     new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
             };
@@ -53,7 +53,7 @@ namespace JWTAuthenticationDemo
             var tokenKey = Encoding.ASCII.GetBytes(key);
             var jwtSecurityToken = new JwtSecurityToken(
                     claims: claims,
-                    expires: DateTime.UtcNow.AddSeconds(20),
+                    expires: DateTime.UtcNow.AddSeconds(15),
                     signingCredentials: new SigningCredentials(
                                     new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
                 );
@@ -93,7 +93,7 @@ namespace JWTAuthenticationDemo
                 {
                     new Claim(ClaimTypes.Name, username)
                 }),
-                Expires = DateTime.UtcNow.AddSeconds(20),
+                Expires = DateTime.UtcNow.AddSeconds(15),
                 SigningCredentials = new SigningCredentials(
                                     new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
             };
